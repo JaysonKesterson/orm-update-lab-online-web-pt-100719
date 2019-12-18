98a1003fb16bea417e0228e1f2c734b586adcebe
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-12-18 21:31:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,8 +4,10 @@ class Student
 
   attr_accessor :id, :name, :grade
   
-  def initialize
-    
+  def initialize(id=nil,name,grade)
+    @id = id
+    @name = name
+    @grade = grade
   end
 
   def self.new_from_db(row)
